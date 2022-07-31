4c01a947f700cf3d844379c1ab218b7f9dc7b206
Tatsuya4610 <tatsuya7610@icloud.com>
2022-07-31 14:20:08 +0900

    ユーザー管理APIのの実装

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth import (
     get_user_model,
     authenticate,
 )
-from django.utils.translation import gettext as _ #翻訳
+from django.utils.translation import gettext as _  # 翻訳
 
 from rest_framework import serializers
 
@@ -21,6 +21,17 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
