69cca358a7afbf05b7c4eabf227c785cc6f4c4c1
Tatsuya4610 <tatsuya7610@icloud.com>
2022-08-01 00:07:22 +0900

    レシピ作成のAPI実装

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -26,3 +26,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer  # 一覧
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
